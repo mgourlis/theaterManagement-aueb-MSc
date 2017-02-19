8892df75a9b78c581eb6611584beb5345318886d
Trapenzanlidis <Trapenzanlidis@LCARS-ELINA>
2017-02-19 21:17:53 +0200

    junit created for UserTest.java also, added UserServices

--- src/main/java/gr/aueb/mscis/theater/service/UserService.java
+++ src/main/java/gr/aueb/mscis/theater/service/UserService.java
@@ -1,6 +1,14 @@
 package gr.aueb.mscis.theater.service;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+import javax.persistence.EntityNotFoundException;
+
+import gr.aueb.mscis.theater.model.User;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Myron on 18/2/2017.
@@ -8,4 +16,71 @@ import javax.persistence.EntityManager;
 public class UserService {
 
     EntityManager em;
+
+    public UserService(EntityManager em) {
+    	this.em = em;
+    }
+
+	public User findUserById(int id) {
+		return em.find(User.class, id);
+	}
+    
+	public User newUser(String firstname,
+						String lastName,
+						String email,
+						String password)
+	{
+		User user = new User(firstname, lastName, email, password);
+		
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		em.persist(user);
+		tx.commit();
+		
+		return user;
+	}
+
+    public List<User> findUserByEmail(String email)
+    {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        List<User> results = null;
+        try {
+            results = em.createQuery("select user from User user where user.email = :userEmail").setParameter("userEmail", email).getResultList();
+            tx.commit();
+        }
+        catch (NoResultException ex) {
+            tx.rollback();
+        }
+        return results;
+    }
+	
+    public List<User> findUserByPassword(String password)
+    {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        List<User> results = null;
+        try {
+            results = em.createQuery("select user from User user where user.password = :pass").setParameter("pass", password).getResultList();
+            tx.commit();
+        }
+        catch (NoResultException ex) {
+            tx.rollback();
+        }
+        return results;
+    }
+    
+	public User saveUser(User user)
+	{
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		if (user.getId() != NULL) {
+            user = em.merge(user);
+		}
+		else
+			em.persist(user);
+
+		tx.commit();
+        return user;
+	}
 }
---
+++ src/test/java/gr/aueb/mscis/theater/model/UserTest.java
@@ -0,0 +1,117 @@
+package gr.aueb.mscis.theater.model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+/**
+ * Created by Myron on 18/2/2017.
+ */
+public class UserTest {
+	
+	User user;
+
+	@Before
+    public void setUp() throws Exception {
+		user = new User("ELEFTHERIA", "TRAPEZANLIDOU", "el@aueb.gr", "pass!");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+	@Test
+    public void getFirstName() throws Exception {
+    	Assert.assertEquals("ELEFTHERIA", user.getFirstName());
+    }
+
+    @Test
+    public void getLastName() throws Exception {
+    	Assert.assertEquals("TRAPEZANLIDOU", user.getLastName());
+    }
+	
+    @Test
+    public void setFirstName() throws Exception {
+    	user.setFirstName("ELINA");
+    	Assert.assertEquals("ELINA", user.getFirstName());
+    }
+
+    @Test
+    public void setLastName() throws Exception {
+    	user.setLastName("TRAPEZALIDOU");
+    	Assert.assertEquals("TRAPEZALIDOU", user.getLastName());
+    }
+
+    @Test
+    public void getPassword() throws Exception {
+    	Assert.assertEquals("pass!", user.getPassword());
+    }
+    
+    @Test
+    public void setPassword() throws Exception {
+    	user.setPassword("password");
+    	Assert.assertEquals("password", user.getPassword());
+    }
+
+//    @Test
+//    public void setEmail() throws Exception {
+//
+//    }
+
+    @Test
+    public void passwordLessThan8() throws Exception {
+    	Assert.assertFalse(user.isPasswordValid());
+    }
+    
+    @Test
+    public void passwordWithoutNumbers() throws Exception {
+    	user.setEmail("password");
+    	Assert.assertFalse(user.isPasswordValid());
+    }
+    
+    @Test
+    public void passwordValidSize8() throws Exception {
+    	user.setEmail("pass1wor");
+    	Assert.assertTrue(user.isPasswordValid());
+    }
+    
+    @Test
+    public void passwordValidSizeMoreThan8() throws Exception {
+    	user.setEmail("2pass1wor");
+    	Assert.assertTrue(user.isPasswordValid());
+    }
+    
+    @Test
+    public void getEmail() throws Exception {
+    	Assert.assertEquals("el@aueb.gr", user.getEmail());
+    }
+
+    @Test
+    public void isEqual() throws Exception {
+		User newUser = new User("x", "y", "el@aueb.gr", "p");
+        assertTrue(user.equals(newUser));
+    }
+    
+    @Test
+    public void notEqual() throws Exception {
+		User newUser = new User("x", "y", "el@auebgr", "p");
+        assertFalse(user.equals(newUser));
+    }
+
+    @Test
+    public void notEqualEmail() throws Exception {
+		User newUser = new User("ELEFTHERIA", "TRAPEZANLIDOU", "el@auebgr", "pass!");
+        assertFalse(user.equals(newUser));
+    }
+    
+//    @Test
+//    public void getToken() throws Exception {
+//
+//    }
+}
