d046fe9c66bab8a0c119b0e7d30bb023d31b33cf
Trapenzanlidis <Trapenzanlidis@LCARS-ELINA>
2017-02-19 21:22:08 +0200

    PurchaseService added

--- src/main/java/gr/aueb/mscis/theater/service/PurchaseService.java
+++ src/main/java/gr/aueb/mscis/theater/service/PurchaseService.java
@@ -1,6 +1,13 @@
 package gr.aueb.mscis.theater.service;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+
+import gr.aueb.mscis.theater.model.Purchase;
 
 /**
  * Created by Myron on 18/2/2017.
@@ -8,4 +15,56 @@ import javax.persistence.EntityManager;
 public class PurchaseService {
 
     EntityManager em;
+    
+    public PurchaseService(EntityManager em) {
+    	this.em = em;
+    }
+	
+	public Purchase newPurchase(Date date,
+								String wayOfPurchase,
+								Integer quantity,
+								Double totalAmount)
+	{
+		Purchase  purchase = new Purchase(date, wayOfPurchase, quantity, totalAmount);
+		
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		em.persist(purchase);
+		tx.commit();
+		
+		return purchase;
+	}
+	
+	public Purchase findPurchaseById(int id) {
+		return em.find(Purchase.class, id);
+	}
+
+    public List<Purchase> findPurchaseByDate(Date date) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        List<Purchase> results = null;
+        try {
+            results = em.createQuery("select purchase from Purchase purchase where purchase.date = :currentDate").setParameter("currentDate", date).getResultList();
+            tx.commit();
+        }
+        catch (NoResultException ex) {
+            tx.rollback();
+        }
+        return results;
+    }
+	
+    public List<Purchase> findPurchaseBywayOfPurchase(String wayOfPurchase) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        List<Purchase> results = null;
+        try {
+            results = em.createQuery("select purchase from Purchase purchase where purchase.wayOfPurchase :wayOfPurchase").setParameter("wayOfPurchase", wayOfPurchase).getResultList();
+            tx.commit();
+        }
+        catch (NoResultException ex) {
+            tx.rollback();
+        }
+        return results;
+    }
+    
 }
