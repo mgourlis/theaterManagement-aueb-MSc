dc1466eab1c09a1a60f70ab7be9552f448edd457
mgourlis <myrgourlis@gmail.com>
2017-02-24 01:12:33 +0200

    Test cases hallmanagement

--- src/test/java/gr/aueb/mscis/theater/useCases/PaymentProviderStub.java
+++ src/test/java/gr/aueb/mscis/theater/useCases/PaymentProviderStub.java
@@ -7,7 +7,7 @@ import gr.aueb.mscis.theater.service.PaymentProvider;
  */
 public class PaymentProviderStub implements PaymentProvider {
     @Override
-    public boolean makePayment(String cardId, int endMonth, int endYear, int cvv, String ownerName) {
+    public boolean makePayment(String cardId, int endMonth, int endYear, int cvv, String ownerName, double amount) {
         if(cardId != null || !cardId.equals(""))
             return true;
         else return false;
