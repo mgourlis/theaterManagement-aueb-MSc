c5a6a4ea2a84e2d230278bcaaa96c48d639eb443
mgourlis <myrgourlis@gmail.com>
2017-02-24 01:12:12 +0200

    Test cases hallmanagement

--- src/main/java/gr/aueb/mscis/theater/service/PaymentProvider.java
+++ src/main/java/gr/aueb/mscis/theater/service/PaymentProvider.java
@@ -5,6 +5,6 @@ package gr.aueb.mscis.theater.service;
  */
 public interface PaymentProvider {
 
-    public boolean makePayment(String cardId, int endMonth, int endYear, int cvv, String ownerName);
+    public boolean makePayment(String cardId, int endMonth, int endYear, int cvv, String ownerName, double amount);
 
 }
