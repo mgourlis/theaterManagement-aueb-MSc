c350512871725c66c6d986860996d86aab70011f
Trapenzanlidis <Trapenzanlidis@LCARS-ELINA>
2017-03-21 02:09:14 +0200

    Removed some comments that accidentally were committed

--- src/test/java/gr/aueb/mscis/theater/Resource/PurchaseResourceTest.java
+++ src/test/java/gr/aueb/mscis/theater/Resource/PurchaseResourceTest.java
@@ -102,30 +102,3 @@ public class PurchaseResourceTest extends TheaterResourceTest {
         //mporw na traviksw ta tickets kai to purchase apo ti vasi gia na dw oti ftiaxtikan...
     }
 }
-
-//@Test
-//	public void testCreateNewLoan() {
-//
-//		// find a borrower through search
-//		List<Borrower> foundBorrowers = findBorrowerByLastName("Διαμαντίδης");
-//		Assert.assertEquals(1, foundBorrowers.size());
-//
-//		Borrower ndia = foundBorrowers.get(0);
-//
-//		LoanRequestInfo loanInfo = new LoanRequestInfo(Initializer.UML_REFACTORING_ID, ndia.getBorrowerNo());
-//
-//		Response response = target(LibraryUri.LOANS).request()
-//				.post(Entity.entity(loanInfo, MediaType.APPLICATION_JSON));
-//
-//		Assert.assertEquals(response.getStatus(), Status.CREATED.getStatusCode());
-//		// assert the existence of the location element
-//		String location = response.getHeaderString(HttpHeaders.LOCATION);
-//		Assert.assertNotNull(location);
-//
-//		// assert properties of new loan
-//		Loan newLoan = findPendingLoanForItem(Initializer.UML_REFACTORING_ID);
-//		Assert.assertNotNull(newLoan);
-//		Assert.assertEquals(Initializer.UML_REFACTORING_ID, newLoan.getItem().getItemNumber());
-//		Assert.assertEquals(ndia.getBorrowerNo(), newLoan.getBorrower().getBorrowerNo());
-//
-//	}
